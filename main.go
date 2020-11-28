d7a7a0148d4bff29825d72453505bd2edb1177ac
m19e <nexus.me7290@gmail.com>
2020-11-28 23:58:39 +0900

    Success media tweet

--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"bot/keys"
+	"bot/media"
 	"fmt"
 	"log"
+	"net/url"
 )
 
 func main() {
 	api := keys.GetTwitterApi()
 
+	base64String := media.LoadEncodedMediaString("./subs/image.png")
+
+	media, _ := api.UploadMedia(base64String)
+
+	v := url.Values{}
+	v.Add("media_ids", media.MediaIDString)
+
 	text := "test tweet"
-	tweet, err := api.PostTweet(text, nil)
+	tweet, err := api.PostTweet(text, v)
 	if err != nil {
 		log.Fatal(err)
 	}
