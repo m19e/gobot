3bb3f0be34660a7e7375ff61c2257925de8fef21
m19e <nexus.me7290@gmail.com>
2020-11-29 04:56:37 +0900

    Read subs dir, tweet with numbering

--- main.go
+++ main.go
@@ -3,33 +3,73 @@ package main
 import (
 	"bot/keys"
 	"bot/media"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
+const subsDir = "subs/"
+
+var subs []string
+var shuffledSubs []string
+
+func init() {
+	filepath.Walk(subsDir, appendSubs)
+	shuffledSubs = shuffled(subs)
+}
+
+func appendSubs(path string, info os.FileInfo, err error) error {
+	if info.IsDir() {
+		return nil
+	}
+	subs = append(subs, strings.Join(strings.Split(path, subsDir), ""))
+	return nil
+}
+
+func shuffled(data []string) []string {
+	n := len(data)
+	r := make([]string, n)
+	copy(r, data)
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(n, func(i, j int) { r[i], r[j] = r[j], r[i] })
+	return r
+}
+
 func main() {
-	// chStop := make(chan int, 1)
-	// TimerFunc(chStop)
+	chStop := make(chan int, 1)
+	TimerFunc(chStop)
 
-	// time.Sleep(time.Hour * 24 * 365)
-	// chStop <- 0
+	time.Sleep(time.Minute * 2)
+	chStop <- 0
 
-	// close(chStop)
+	close(chStop)
 
-	// time.Sleep(time.Second * 1)
+	// sc := make(chan os.Signal, 1)
+	// signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	// <-sc
 
-	// log.Println("Application End.")
+	time.Sleep(time.Second * 1)
+	log.Println("Application End.")
 }
 
 func TimerFunc(stopTimer chan int) {
 	go func() {
-		ticker := time.NewTicker(30 * time.Minute)
+		ticker := time.NewTicker(1 * time.Hour)
 
 		for {
 			select {
 			case <-ticker.C:
-				post()
+				if len(shuffledSubs) == 0 {
+					shuffledSubs = shuffled(subs)
+				}
+				log.Println(shuffledSubs[0])
+				post(subsDir, shuffledSubs[0])
+				shuffledSubs = shuffledSubs[1:]
 
 			case <-stopTimer:
 				log.Println("Timer stop.")
@@ -40,17 +80,17 @@ func TimerFunc(stopTimer chan int) {
 	}()
 }
 
-func post() {
+func post(dir, fp string) {
 	api := keys.GetTwitterApi()
 
-	base64String := media.LoadEncodedMediaString("./subs/image.png")
+	base64String := media.LoadEncodedMediaString(fmt.Sprintf("%s%s", dir, fp))
 
 	media, _ := api.UploadMedia(base64String)
 
 	v := url.Values{}
 	v.Add("media_ids", media.MediaIDString)
 
-	text := "test tweet"
+	text := fmt.Sprintf("#%s", strings.Split(fp, "/")[0])
 	tweet, err := api.PostTweet(text, v)
 	if err != nil {
 		log.Fatal(err)
