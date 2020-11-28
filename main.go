22746d36cdbc2415d0f1547d6c629bfe46109eb2
m19e <nexus.me7290@gmail.com>
2020-11-29 01:10:33 +0900

    WIP

--- main.go
+++ main.go
@@ -5,9 +5,42 @@ import (
 	"bot/media"
 	"log"
 	"net/url"
+	"time"
 )
 
 func main() {
+	// chStop := make(chan int, 1)
+	// TimerFunc(chStop)
+
+	// time.Sleep(time.Hour * 24 * 365)
+	// chStop <- 0
+
+	// close(chStop)
+
+	// time.Sleep(time.Second * 1)
+
+	// log.Println("Application End.")
+}
+
+func TimerFunc(stopTimer chan int) {
+	go func() {
+		ticker := time.NewTicker(30 * time.Minute)
+
+		for {
+			select {
+			case <-ticker.C:
+				post()
+
+			case <-stopTimer:
+				log.Println("Timer stop.")
+				ticker.Stop()
+				break
+			}
+		}
+	}()
+}
+
+func post() {
 	api := keys.GetTwitterApi()
 
 	base64String := media.LoadEncodedMediaString("./subs/image.png")
